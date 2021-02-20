f9e722a3f4e017b1712bcad7d250bd470038151a
Felipe Alves <usermestre@hotmail.com>
2021-02-20 12:27:00 -0300

    Final Project V1

--- githubjs/src/pages/Repository/index.tsx
+++ githubjs/src/pages/Repository/index.tsx
@@ -1,12 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useRouteMatch } from 'react-router-dom';
-import { Header } from './style';
+import { Header, HeaderInfoRepository, RepositoryInfoUser } from './style';
 import Logo from '../../assets/images/Logo.svg'
-import { FiChevronLeft } from "react-icons/fi"
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi"
 import { Link } from 'react-router-dom';
+import Tiago from '../../assets/images/Tiago.png';
+import api  from '../../services/api';
 
 interface RepositoryParms {
   repository: string;
+}
+
+export interface IRepository {
+  full_name: string;
+  description: string;
+  stargazers_count: number;
+  forks_count: number;
+  open_issues_count: number;
+  owner: {
+    login: string;
+    avatar_url: string;
+  }
+}
+
+interface Issue {
+  id: number;
+  title: string;
+  html_url: string;
+  user: {
+    login: string;
+  }
 }
 
 // const Repository: React.FC = () => {
@@ -15,9 +38,25 @@ interface RepositoryParms {
 // };
 
 const Repository: React.FC = () => {
+
+  const [IRepository, setIRepository] = useState<IRepository | null>(null);
+  const [issues, setIssues] = useState<Issue[]>([]);
+  
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   const { params } = useRouteMatch<RepositoryParms>();
+
+  useEffect(() => {
+    api.get(`repos/${params.repository}`).then((response) => {
+      setIRepository(response.data)
+    });
+
+    api.get(`repos/${params.repository}/issues`).then((response) => {
+      setIssues(response.data)
+    });
+  }, [params.repository])
+  
   return(
+    <>
     <Header>
       <img src={Logo} alt="GitHub Explorer"/>
       <Link to="/">
@@ -25,6 +64,42 @@ const Repository: React.FC = () => {
         Voltar
       </Link>
     </Header>
+    <HeaderInfoRepository>
+      <header>
+      <img src={IRepository?.owner.avatar_url} alt={IRepository?.owner.login}/>
+      <div>
+      <strong>{IRepository?.full_name}</strong>
+      <p>{IRepository?.description}</p>
+      </div>
+      </header>
+          <ul>
+            <li>
+              <strong>{IRepository?.stargazers_count}</strong>
+              <span>Stars</span>
+            </li>
+            <li>
+              <strong>{IRepository?.forks_count}</strong>
+              <span>Forks</span>
+            </li>
+            <li>
+              <strong>{IRepository?.open_issues_count}</strong>
+              <span>Issues Abertas</span>
+            </li>
+          </ul>
+    </HeaderInfoRepository>
+    <RepositoryInfoUser>
+    {issues.map(issue => (
+          <a key={issue.id} href={issue.html_url}>
+            <div>
+              <strong>{issue.title}</strong>
+              <p>{issue.user.login}</p>
+            </div>
+
+            <FiChevronRight size={20} />
+          </a>
+        ))}
+    </RepositoryInfoUser>
+    </>
   );
 };
 
--- githubjs/src/pages/Repository/style.ts
+++ githubjs/src/pages/Repository/style.ts
@@ -22,3 +22,120 @@ export const Header = styled.header `
     }
   }
 `;
+
+
+export const HeaderInfoRepository = styled.section`
+    margin-top: 80px;
+
+    header {
+      display: flex;
+      align-items: center;
+
+      img {
+        width: 124px;
+        height: 124px;
+        border-radius:50%;
+      }
+      div {
+        margin-left: 24px;
+
+        strong{
+          font-size: 38px;
+          color: #3d3d4d;
+          font-family: 'Roboto', sans-serif;
+        }
+        p{
+          margin-top: 4px;
+          font-size: 18px;
+          color: #737380;
+          font-family: 'Roboto', sans-serif;
+          font-weight: 400;
+        }
+      }
+    }
+
+    ul{
+      list-style: none;
+      display: flex;
+      margin-top: 40px;
+      li {
+
+        & + li {
+          margin-left: 80px;
+        }
+        strong {
+          display: block;
+          color: #3d3d4d;
+          font-size: 36px;
+        }
+        span {
+          display: block;
+          color: #6C6C80;
+          margin-top: 4px;
+        }
+      }
+    }
+
+    /* img{
+      width: 124px;
+      height: 124px;
+      border-radius: 50%;
+    }
+    
+    strong{
+      font-size: 36px;
+      color: #3D3D4D;
+      font-weight: 700;
+    } */
+
+`;
+
+export const RepositoryInfoUser = styled.div `
+  margin-top: 80px;
+  height: 112px;
+
+  a {
+    background: #FFF;
+    border-radius: 5px;
+    padding: 24px;
+    display: block;
+    text-decoration:none;
+    display:flex;
+    align-items:center;
+    transition: transform 0.2s;
+    
+    & + a {
+      margin-top: 10px;
+    }
+
+     &:hover {
+       transform: translateX(10px);
+     }
+  }
+
+  img {
+    width: 64px;
+    height: 64px;
+    border-radius: 50%;
+  }
+
+  div {
+    margin: 0 16px;
+    flex:1;
+
+    strong{
+      font-size: 20px;
+      color: #3D3D4D;
+    }
+    p {
+      font-size:18px;
+      margin-top:4px;
+      color: #A8A8B3;
+    }
+  }
+  svg {
+    margin-left: auto;
+    color: #C9C9D4;
+  }
+`;
+
